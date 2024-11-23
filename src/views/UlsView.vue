8e113d246ce051b4a20525b9695039d4bf45b829
Gaby8973621 <gabriela472151@gmail.com>
2024-11-22 18:00:15 -0600

    botiquin
    primer proyecto

---
+++ src/views/UlsView.vue
@@ -0,0 +1,25 @@
+<!-- uls.vue -->
+<template>
+    <div class="uls-container">
+      <h1>Misión y Visión de la Universidad Luterana Salvadoreña</h1>
+      <section class="mission">
+        <h2>Misión</h2>
+        <p>La Universidad Luterana Salvadoreña, es una institución que ofrece educación superior de calidad a todos los sectores de la sociedad, poniendo especial énfasis en los más vulnerables. Basándose en un modelo participativo y cumpliendo con sus funciones básicas: docencia, investigación, 
+          proyección social e internacionalización, contribuye al desarrollo social, político y económico sustentable del país.</p>
+      </section>
+      <section class="vision">
+        <h2>Visión</h2>
+        <p>Ser una institución con altos estándares de calidad, capaz de responder a través de sus funciones básicas a los retos emergentes de la sociedad, formando profesionales con capacidad técnico-científica, con vocación de servicio, principios morales sólidos, proactivos, dinámicos,
+           que se interesen por aportar en la construcción de una sociedad más justa.</p>
+      </section>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'UlsView',
+  };
+  </script>
+  
+  <style src="@/assets/viewstyles/UlsView.css" scoped></style>
+
